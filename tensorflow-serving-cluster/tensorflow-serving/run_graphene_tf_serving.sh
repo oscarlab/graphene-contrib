76167525559f8c914bbb568e08374133b4b4a573
RodgerZhu <yunge.zhu@intel.com>
2021-04-06 08:57:05 +0800

    Update run_graphene_tf_serving.sh

--- tensorflow-serving-cluster/tensorflow-serving/run_graphene_tf_serving.sh
+++ tensorflow-serving-cluster/tensorflow-serving/run_graphene_tf_serving.sh
@@ -8,7 +8,7 @@ function usage_help() {
     echo -e "options:"
     echo -e "  -h Display help"
     echo -e "  -i {image_id}"
-    echo -e "  -h {host_ports}"
+    echo -e "  -p {host_ports}"
     echo -e "  -m {model_name}"
     echo -e "  -s {ssl_config_file}"
     echo -e "  -a {attestation_hosts}"
@@ -35,7 +35,7 @@ https_proxy=""
 no_proxy=""
 
 # Override args
-while getopts "h?r:a:b:c:d:e:f:" OPT; do
+while getopts "h?r:i:p:m:s:a:e:" OPT; do
     case $OPT in
         h|\?)
             usage_help
@@ -45,7 +45,7 @@ while getopts "h?r:a:b:c:d:e:f:" OPT; do
             echo -e "Option $OPTIND, image_id = $OPTARG"
             image_id=$OPTARG
             ;;
-        h)
+        p)
             echo -e "Option $OPTIND, host_ports = $OPTARG"
             host_ports=$OPTARG
             ;;
